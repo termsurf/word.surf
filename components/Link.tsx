56f28b903f4c386ee87eac3d30b142e7f0f61a60
Lance Pollard <lp@elk.fm>
2024-09-26 18:35:42 -0700

    save

--- components/Link.tsx
+++ components/Link.tsx
@@ -24,11 +24,11 @@ function Link({
           'shadow-small1 flex flex-col gap-8 bg-gray-100 text-left p-16 h-full rounded-sm w-full',
         )}
       >
-        <T className="block font-semibold lowercase text-base sm:text-base-large text-gray-300">
+        <T className="block font-semibold lowercase text-lg sm:text-lg-large text-gray-300">
           {title}
         </T>
         {description && (
-          <T className="block text-gray-300 text-base sm:text-base-large">
+          <T className="block text-gray-300 text-lg sm:text-lg-large">
             {description}
           </T>
         )}
@@ -43,11 +43,11 @@ function Link({
         'shadow-small1 hover:shadow-small2 flex flex-col gap-8 bg-gray-50 [&_span]:hover:text-violet-600 [&>div]:transition-colors transition-all duration-200 text-left p-16 h-full rounded-sm w-full',
       )}
     >
-      <T className="block font-semibold lowercase text-base sm:text-base-large transition-colors">
+      <T className="block font-semibold lowercase text-lg sm:text-lg-large transition-colors">
         {title}
       </T>
       {description && (
-        <T className="block text-gray-600 transition-colors text-base sm:text-base-large">
+        <T className="block text-gray-600 transition-colors text-lg sm:text-lg-large">
           {description}
         </T>
       )}
